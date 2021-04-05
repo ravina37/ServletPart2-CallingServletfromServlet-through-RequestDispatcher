7664855c15b01dc815ba4e8a43bdab5f7c0d0668
ravina <t.ravinacbe@gmail.com>
2021-04-05 11:26:18 +0530

    first commit

--- src/com/ravina/Value2Servlet.java
+++ src/com/ravina/Value2Servlet.java
@@ -8,14 +8,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class Value2Servlet extends HttpServlet {
-	
-	public void doGet(HttpServletRequest req,HttpServletResponse res) throws IOException
-	{
-//	    int a=Integer.parseInt(req.getParameter("a"));
-		int a= (int) req.getAttribute("a");
-PrintWriter out=res.getWriter();
-out.println("calling servlet from servlet through request Dispatcher");
-out.println("value of a is:"+a);
+
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		int a = Integer.parseInt(req.getParameter("a"));
+
+		res.setContentType("text/html");
+		PrintWriter out = res.getWriter();
+		out.println("<h1>Result:</h1>");
+		out.println("value of a:" + a);
+
+		System.out.println("Send redirect method worked!");
+		System.out.println(" worked!");
 	}
 
 }
--- src/com/ravina/ValueServlet.java
+++ src/com/ravina/ValueServlet.java
@@ -2,8 +2,6 @@ package com.ravina;
 
 import java.io.IOException;
 
-
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -13,13 +11,9 @@ public class ValueServlet extends HttpServlet{
 	
 	public void doGet(HttpServletRequest req,HttpServletResponse res) throws IOException, ServletException
 	{
-	int a=Integer.parseInt(req.getParameter("a"));
-	req.setAttribute("a", a);
-		
-		RequestDispatcher rd=req.getRequestDispatcher("/value2");
-		rd.forward(req, res);
-//PrintWriter out=res.getWriter();
-//out.println("Value of a is:"+a);
+		int a=Integer.parseInt(req.getParameter("a"));
+		res.sendRedirect("value2?a="+a);
+
 	}
 
 }
